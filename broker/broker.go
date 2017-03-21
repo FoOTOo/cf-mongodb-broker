346dbc47d133809e523a21dbec58e3451315e592
ultragtx <ultragtx@gmail.com>
2017-03-21 14:13:57 +0800

    Basic broker

---
+++ broker/broker.go
@@ -0,0 +1,84 @@
+package broker
+
+import (
+	"context"
+
+	"github.com/pivotal-cf/brokerapi"
+)
+
+type MongoServiceBroker struct{}
+
+func (mongoServiceBroker *MongoServiceBroker) Services(context context.Context) []brokerapi.Service {
+	// TODO: read config
+
+	free := true
+
+	plans := []brokerapi.ServicePlan{
+		brokerapi.ServicePlan{
+			ID:          "SOME-UUID-98769-standard", // TODO: better uuid
+			Name:        "standard",
+			Description: "Standard mongodb plan",
+			Free:        &free,
+			//Bindable:
+			//Metadata:
+		},
+	}
+
+	services := []brokerapi.Service{
+		brokerapi.Service{
+			ID:            "SOME-UUID-98769-mongodb-service", // TODO: better uuid
+			Name:          "footoo-mongodb",
+			Description:   "A in development mongodb service",
+			Bindable:      true,
+			Tags:          []string{"FoOTOo", "mongodb"},
+			PlanUpdatable: false,
+			Plans:         plans,
+			//Requires
+			//Metadata
+			//DashboardClient
+		},
+	}
+
+	return services
+}
+
+func (mongoServiceBroker *MongoServiceBroker) Provision(context context.Context, instanceID string, serviceDetails brokerapi.ProvisionDetails, asyncAllowed bool) (brokerapi.ProvisionedServiceSpec, error) {
+	spec := brokerapi.ProvisionedServiceSpec{}
+
+	// TODO:
+
+	return spec, nil
+}
+
+func (mongoServiceBroker *MongoServiceBroker) Deprovision(context context.Context, instanceID string, details brokerapi.DeprovisionDetails, asyncAllowed bool) (brokerapi.DeprovisionServiceSpec, error) {
+	spec := brokerapi.DeprovisionServiceSpec{}
+
+	// TODO:
+
+	return spec, nil
+}
+
+func (mongoServiceBroker *MongoServiceBroker) Bind(context context.Context, instanceID, bindingID string, details brokerapi.BindDetails) (brokerapi.Binding, error) {
+	binding := brokerapi.Binding{}
+
+	// TODO:
+
+	return binding, nil
+}
+
+func (mongoServiceBroker *MongoServiceBroker) Unbind(context context.Context, instanceID, bindingID string, details brokerapi.UnbindDetails) error {
+	// TODO: nil
+
+	return nil
+}
+
+// LastOperation ...
+// If the broker provisions asynchronously, the Cloud Controller will poll this endpoint
+// for the status of the provisioning operation.
+func (mongoServiceBroker *MongoServiceBroker) LastOperation(context context.Context, instanceID, operationData string) (brokerapi.LastOperation, error) {
+	return brokerapi.LastOperation{}, nil
+}
+
+func (mongoServiceBroker *MongoServiceBroker) Update(context context.Context, instanceID string, details brokerapi.UpdateDetails, asyncAllowed bool) (brokerapi.UpdateServiceSpec, error) {
+	return brokerapi.UpdateServiceSpec{}, nil
+}
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/pivotal-cf/brokerapi"
+
+	"code.cloudfoundry.org/lager"
+	"github.com/ultragtx/mongo-broker/broker"
+)
+
+const (
+	BrokerName     = "mongodb-broker"
+	BrokerUsername = "mongodb-broker-user"
+	BrokerPassword = "mongodb-broker-password"
+)
+
+func main() {
+	brokerLogger := lager.NewLogger(BrokerName)
+	brokerLogger.RegisterSink(lager.NewWriterSink(os.Stdout, lager.DEBUG))
+	brokerLogger.RegisterSink(lager.NewWriterSink(os.Stderr, lager.ERROR))
+
+	brokerLogger.Info("Starting Mongodb broker")
+
+	// TODO: config file
+
+	serviceBroker := &broker.MongoServiceBroker{}
+
+	brokerCredentials := brokerapi.BrokerCredentials{
+		Username: BrokerUsername,
+		Password: BrokerPassword,
+	}
+
+	// broker
+	brokerAPI := brokerapi.New(serviceBroker, brokerLogger, brokerCredentials)
+
+	// authWrapper := auth.NewWrapper(brokerCredentials.Username, brokerCredentials.Password)
+	// TODO: ??? /instance /debug
+
+	http.Handle("/", brokerAPI)
+
+	brokerLogger.Fatal("http-listen", http.ListenAndServe(":"+"9876", nil)) // TODO: config
+}
